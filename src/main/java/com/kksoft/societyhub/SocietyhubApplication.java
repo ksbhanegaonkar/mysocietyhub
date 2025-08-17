9a21ce66eba0548bb714d8953baa1fbd134a28f8
Kedar Bhanegaonkar <ksbhanegaonkar@gmail.com>
2025-08-17 21:29:25 +0530

    Initial setup spring boot application

---
+++ src/main/java/com/kksoft/societyhub/SocietyhubApplication.java
@@ -0,0 +1,13 @@
+package com.kksoft.societyhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocietyhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocietyhubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kksoft/societyhub/SocietyhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kksoft.societyhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocietyhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
